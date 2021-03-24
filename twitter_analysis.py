f4fc70183a58ae721b5c5e8c8ccff878c62520bd
Anjana <48853197+Anjanavenkat21@users.noreply.github.com>
2021-03-24 16:19:11 +0530

    Add files via upload

---
+++ twitter_analysis.py
@@ -0,0 +1,57 @@
+import tweepy
+from textblob import TextBlob
+import string
+from collections import Counter
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# Step 1 - Authenticate
+consumer_key = "iZMzhXXwROneN2lG3uPL7CNio"
+consumer_secret = "gw6lDI2O2vyf9G5IHuIFee1knDOdVzUEEMzSSZK0buntrkpGyz"
+
+access_token = "1074330164014678017-2vBlAStmCCGLhl74B4SpVHn7b7QM03"
+access_token_secret = "SU6saxwQWYtc85hJQPpZIkjwk82wRqqjffBT5r3c4gphW"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Step 3 - Retrieve Tweets
+public_tweets = api.search(input("enter to search="))
+
+# CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+# and label each one as either 'positive' or 'negative', depending on the sentiment
+# You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+
+    # Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
+
+# Create textblob objects of the tweets
+sentiment_objects = [TextBlob(tweet) for tweet in tweet.text]
+
+sentiment_objects[0].polarity, sentiment_objects[0]
+# Create list of polarity valuesx and tweet text
+sentiment_values = [[tweet.sentiment.polarity, str(tweet)] for tweet in sentiment_objects]
+
+sentiment_values[0]
+# Create dataframe containing the polarity value and tweet text
+sentiment_df = pd.DataFrame(sentiment_values, columns=["polarity", "tweet"])
+
+sentiment_df.head()
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Plot histogram of the polarity values
+sentiment_df.hist(bins=[-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1],
+             ax=ax,
+             color="blue")
+
+plt.title("Sentiments from Tweets ")
+plt.show()
